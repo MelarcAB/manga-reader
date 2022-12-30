a34cd1b92c61a4a359569679d140924274d5b396
Melarc <melarc.ab@gmail.com>
2022-12-30 03:18:22 +0100

    se permite nickname para los usuarios

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,18 +7,27 @@ use Illuminate\Http\Request;
 use App\User;
 use JWTAuth;
 
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 class RegisterController extends Controller
 {
     public function register(Request $request)
     {
         $request->validate([
             'name' => 'required|string',
+            'nickname' => ['required', 'string', 'max:255'],
+
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
 
         $user = new User([
             'name' => $request->name,
+            'nickname' => $request->nickname,
+
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
